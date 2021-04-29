0d196c33afea304a32dbdbe9cc45e4c713f104c2
tempati <dadsbads97@gmail.com>
2021-04-29 16:49:10 +0900

    アプリ作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by 稲葉任哉 on 2021/04/26.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
